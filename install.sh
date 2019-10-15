0751e25b0e11a5226e7c2bd70ac721909f378fe7
Guillaume Delanoy <guillaume@dela.noy>
2019-10-15 23:03:50 +0200

    install.sh modifié

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ln -s .shells/ps1.txt .ps1.txt
+#   670  ln -s .shells/ps2.txt .ps2.txt
+#   671  ln -s .shells/pscmd.txt .pscmd.txt
+
+#  mv .fzf.* /tmp
+#  673  ln -s .shells/fzf.bash .fzf.bash
+#  674  ln -s .shells/fzf.zsh .fzf.zsh
+
