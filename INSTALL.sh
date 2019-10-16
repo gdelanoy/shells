224f9e9e781e04500ae32350c5ad73d939b4bf8b
guillaume <guillaume@delanoy.fr>
2019-10-16 11:11:05 +0200

    Commit du mercredi 16 octobre 2019 à 11 heures 11 .

--- INSTALL.sh
+++ INSTALL.sh
@@ -7,4 +7,11 @@
 #  mv .fzf.* /tmp
 #  673  ln -s .shells/fzf.bash .fzf.bash
 #  674  ln -s .shells/fzf.zsh .fzf.zsh
+cd /home/$USER/
+[ -f /home/$USER/.zshrc ] && ( echo "Sauvegarde du zshrc existant dans ~/.shells/old/ ..."; mv /home/$USER/.zshrc /home/$USER/.shells/old/zshrc-original-$(date +%F-%T))
+sh -c "$(wget -O- https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+mv /home/$USER/.zshrc /home/$USER/.shells/old/zshrc-original-$(date +%F-%T)
+ln -s /home/$USER/.shells/zshrc .zshrc
 
--- oh-my-zsh/custom/themes/spaceship.zsh-theme
+++
@@ -1,1 +0,0 @@
-/home/guillaume/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme
