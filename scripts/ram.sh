fd23d1f67b73a088b16bc0ecea991aed9f739821
terminalforlife <terminalforlife@yahoo.com>
2019-11-04 02:07:18 +0000

    Updated ram.sh; tidied funcs; RAM display overhaul

--- scripts/ram.sh
+++ scripts/ram.sh
@@ -14,42 +14,43 @@ set -o nounset
 #
 # VARIABLES :
 #
-#Des Couleurs 
-# 
-bold=$(tput bold) 
-underline=$(tput sgr 0 1) 
-reset=$(tput sgr0) 
-purple=$(tput setaf 171) 
-red=$(tput setaf 1) 
-green=$(tput setaf 76) 
-tan=$(tput setaf 3) 
-blue=$(tput setaf 38) 
-# 
+#Des Couleurs
+#
+bold=$(tput bold)
+underline=$(tput sgr 0 1)
+reset=$(tput sgr0)
+purple=$(tput setaf 171)
+red=$(tput setaf 1)
+green=$(tput setaf 76)
+tan=$(tput setaf 3)
+blue=$(tput setaf 38)
+#
 # En-têtes et Logs
-# 
-e_header() { printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"  
-} 
-e_arrow() { printf "➜ $@\n" 
-} 
-e_success() { printf "${green}✔ %s${reset}\n" "$@" 
-} 
-e_error() { printf "${red}✖ %s${reset}\n" "$@" 
-} 
-e_warning() { printf "${tan}➜ %s${reset}\n" "$@" 
-} 
-e_underline() { printf "${underline}${bold}%s${reset}\n" "$@" 
-} 
-e_bold() { printf "${bold}%s${reset}\n" "$@" 
-} 
-e_note() { printf "${underline}${bold}${blue}Note:${reset}  ${blue}%s${reset}\n" "$@" 
-} 
+#
+e_header() { printf "\n${bold}${purple}==========  %s  ==========${reset}\n" "$@"; }
+e_arrow() { printf "➜ $@\n"; }
+e_success() { printf "${green}✔ %s${reset}\n" "$@"; }
+e_error() { printf "${red}✖ %s${reset}\n" "$@"; }
+e_warning() { printf "${tan}➜ %s${reset}\n" "$@"; }
+e_underline() { printf "${underline}${bold}%s${reset}\n" "$@"; }
+e_bold() { printf "${bold}%s${reset}\n" "$@"; }
+e_note() { printf "${underline}${bold}${blue}Note:${reset}  ${blue}%s${reset}\n" "$@"; }
 #
 # FONCTIONS :
 #
 # source /etc/skel/.scripts/functions.sh
 #
-# LE SCRIPT PROPREMENT DIT : 
+# LE SCRIPT PROPREMENT DIT :
 #
 
-ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | awk '{print $1/1024 " MB\t\t" $2}'
+#ps aux | awk '{print $6 "\t" $11}' | sort -rn | head -25 | awk '{print $1/1024 " MB\t\t" $2}'
+
+awk "
+	{
+		M=\$1/1024
+		if(NR<25 && M>1){
+			printf(\"%'7dMB  %s\\n\", M, \$2)
+		}
+	}
+" <<< "$(ps ax -o rss= -o comm= --sort -rss)"
 
