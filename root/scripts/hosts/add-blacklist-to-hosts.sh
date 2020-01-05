c41192127b18d8f376850b5d85487e49d04d422f
Guillaume Delanoy <gdelanoy@gmail.com>
2020-01-05 18:15:35 +0100

    Your processor has taken a ride to Heaven's Gate on the UFO behind Hale-Bopp's comet.

--- root/scripts/hosts/add-blacklist-to-hosts.sh
+++ root/scripts/hosts/add-blacklist-to-hosts.sh
@@ -9,7 +9,7 @@ echo "127.0.0.1   chat.peterschmitt.fr  # Noirlistez-moi !!!"   >> /etc/hosts
 
 # echo "Access to brainspace-wasting sites is restricted BY NOW so that you can get a life. Use Focuswriter instead ..." | wall
 
-sudo -u guillaume DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -t 30000 'Filtrage web activé' 'Access to brainspace-wasting sites is restricted BY NOW so that you can get a life. Use Focuswriter instead ...'
+sudo -u guillaume DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -t 90000 'Web filtering now active' 'Access to brainspace-wasting sites is restricted BY NOW so that you can get a life. Use Focuswriter instead ...'
 
 # To adapt this script change my name to your username in the above line, as well as UID (1000 here) !
 
--- root/scripts/hosts/remove-blacklist-from-hosts.sh
+++ root/scripts/hosts/remove-blacklist-from-hosts.sh
@@ -6,7 +6,7 @@
 sed -i '/Noirlistez-moi/d' /etc/hosts
 
 
-sudo -u guillaume DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -t 30000 'Filtrage web désactivé' 'Access to brainspace-wasting sites is possilbe again. Enjoy your personal lobotomy ...'
+sudo -u guillaume DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus notify-send -t 90000 'Filtrage web désactivé' 'Access to brainspace-wasting sites is possilbe again. Enjoy your personal lobotomy ...'
 
 # To adapt this script change my name to your username in the above line, as well as UID (1000 here) !
 
