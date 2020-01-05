d3e4411804908a49291d348a3284299ece52154e
Guillaume Delanoy <gdelanoy@gmail.com>
2020-01-05 15:24:03 +0100

    Telecommunications is upgrading.

--- root/scripts/hosts/add-blacklist-to-hosts.sh
+++ root/scripts/hosts/add-blacklist-to-hosts.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo "Access to Twitter and Facebook is restricted BY NOW so that you can get a life. Use Focuswriter instead ..." | wall
+echo "127.0.0.1   www.facebook.com      # Noirlistez-moi !!!"  >> /etc/hosts
+echo "127.0.0.1   www.twitter.com       # Noirlistez-moi !!!"   >> /etc/hosts
+echo "127.0.0.1   twitter.com           # Noirlistez-moi !!!"   >> /etc/hosts
+echo "127.0.0.1   chat.peterschmitt.fr  # Noirlistez-moi !!!"   >> /etc/hosts
 
-echo "127.0.0.1   www.facebook.com"  >> /etc/hosts
-echo "127.0.0.1   www.twitter.com"   >> /etc/hosts
+# Usage :  echo "127.0.0.1   @domain_to_blacklist # Noirlistez-moi !!!"   >> /etc/hosts
+
+echo "Access to brainspace-wasting sites is restricted BY NOW so that you can get a life. Use Focuswriter instead ..." | wall
 
 exit
 
--- root/scripts/hosts/remove-blacklist-from-hosts.sh
+++ root/scripts/hosts/remove-blacklist-from-hosts.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sed -i '/www.facebook.com/d' /etc/hosts
-sed -i '/www.twitter.com/d' /etc/hosts
+# sed -i '/www.facebook.com/d' /etc/hosts
+# sed -i '/www.twitter.com/d' /etc/hosts
 
+sed -i '/Noirlistez-moi/d' /etc/hosts
 
 exit
