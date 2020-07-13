833c563ea09a17c24f20fd1daccb00f53aa0c416
Guillaume Delanoy <guillaume@delanoy.fr>
2020-07-14 00:37:27 +0200

    Suspicious pointer corrupted virtual machine

--- scripts/byobu-session-VM.sh
+++ scripts/byobu-session-VM.sh
@@ -22,10 +22,8 @@ if [ -z "$(byobu list-sessions | grep $USER)" ]
  # On crée une nouvelle fenêtre et on la renomme ...
  byobu-tmux new-window
  byobu-tmux rename-window 'Files'
- byobu split-window -v
- byobu-tmux send-keys -t 0 'cd /; fcd' 'C-m'
+ byobu-tmux send-keys 'br' 'C-m'
  # byobu-tmux send-keys -t 1 'ranger' 'C-m'
- byobu-tmux send-keys -t 1 'nnn' 'C-m'
 
  # Une nouvelle fenêtre pour vim avec son explorateur ...
  byobu-tmux new-window
